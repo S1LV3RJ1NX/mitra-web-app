971ef5ba1cf9c15da0e9bc4de0ee57eb55073c6b
s1lv3rj1nx <pratamesh1867@gmail.com>
2020-06-21 20:20:49 +0530

    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+# This file is used to host MITRA bot website
+
+# Imports
 import os, sys
 from flask import Flask, request, render_template
 
+# Registering the app
 app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/', methods = ['GET'])
 def index():
+	"""This function renders the index page on GET request
+	"""
 	return render_template('index.html')
 
 def log(message):
+	"""Useful to log messages to server
+	"""
 	print(message)
 	sys.stdout.flush()
 
